6e40aace711bf519f0e69ac6dd2be732bbc6c4cd
PR <ponnurajan.1314@gmail.com>
2026-03-06 19:58:21 +0530

    added css

--- public/script.js
+++ public/script.js
@@ -1,16 +1,15 @@
 async function getWeather() {
-
     const city = document.getElementById("city").value;
 
     const response = await fetch(`http://localhost:3000/weather/${city}`);
     const data = await response.json();
 
-    document.getElementById("temp").textContent =
-        "Temperature: " + data.main.temp + "°C";
+    document.querySelector("#temp .stat-value").textContent =
+        data.main.temp + "°C";
 
-    document.getElementById("condition").textContent =
-        "Condition: " + data.weather[0].main;
+    document.querySelector("#condition .stat-value").textContent =
+        data.weather[0].main;
 
-    document.getElementById("humidity").textContent =
-        "Humidity: " + data.main.humidity + "%";
+    document.querySelector("#humidity .stat-value").textContent =
+        data.main.humidity + "%";
 }
